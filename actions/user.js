fe7cf7dd926727455ff267a8fa145a483501cc0f
clonalejandro <clonalejandromcb@gmail.com>
2021-03-04 18:35:32 +0100

    Init project

---
+++ actions/user.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: 'user',
+    type: 'post',
+    callback: (req, res) => {
+      //TODO: save User
+    },
+  },
+  {
+    id: 'user',
+    type: 'get',
+    callback: (req, res) => {
+      //TODO: return true || false
+    },
+  },
+  {
+    id: 'user',
+    type: 'put',
+    callback: (req, res) => {
+      //TODO: update User
+    }
+  },
+  {
+    id: 'user',
+    type: 'delete',
+    callback: (req, res) => {
+      //TODO: delete User
+    },
+  },
+]
